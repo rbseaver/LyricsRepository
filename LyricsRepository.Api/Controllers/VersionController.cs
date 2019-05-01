d600d15bca6ca6b39c739fb1e3379e744e8e4e23
Rob Seaver <rob.seaver@gmail.com>
2019-04-30 20:58:43 -0500

    Flesh out basics

---
+++ LyricsRepository.Api/Controllers/VersionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LyricsRepository.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        // GET: api/Version
+        [HttpGet]
+        public string Get()
+        {
+            return string.Empty;
+        }
+    }
+}
