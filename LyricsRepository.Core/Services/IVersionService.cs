0f99b657ea109444d51f8b9f8424fc08b5d601c4
Rob Seaver <rob.seaver@gmail.com>
2019-05-01 21:11:49 -0500

    chore: Integratino test for VersionController

---
+++ LyricsRepository.Core/Services/IVersionService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace LyricsRepository.Core
+{
+    public interface IVersionService
+    {
+        Task<string> GetVersionAsync<T>();
+    }
+}
---
+++ LyricsRepository.Tests.Integration/Api/Controllers/VersionControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using LyricsRepository.Api;
+using LyricsRepository.Tests.Integration.TestHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace LyricsRepository.Tests.Integration.Api.Controllers
+{
+    [TestClass]
+    public class VersionControllerTests
+    {
+        [TestMethod]
+        public async Task ShouldGetVersion()
+        {
+            var expectedVersion = typeof(Startup).Assembly.GetName().Version.ToString();
+
+            using (var client = TestClient.CreateClient())
+            {
+                var response = await client.GetAsync("/api/version");
+
+                var version = await response.Content.ReadAsStringAsync();
+
+                response.EnsureSuccessStatusCode();
+                version.Should().Be(expectedVersion);
+            }
+        }
+    }
+}
