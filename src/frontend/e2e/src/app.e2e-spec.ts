5be5691fe2d65e9f534500c26f9942e7151206a7
Rob Seaver <rob.seaver@gmail.com>
2019-08-19 19:39:12 -0500

    chore: Correct fallout from not running in src/ folder

--- src/frontend/e2e/src/app.e2e-spec.ts
+++ src/frontend/e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+import {} from 'jasmine';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,7 +11,9 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to frontend!');
+    page.getTitleText().then((t) => {
+      expect(t).toEqual('Welcome to frontend!');
+    });
   });
 
   afterEach(async () => {
